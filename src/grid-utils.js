5594a94c3c4930beb4b51a78f9d82905a6d886a7
Orphée Besson <orphee.besson@etudiant.univ-lr.fr>
2021-06-30 22:20:39 +0200

    📐

--- src/grid-utils.js
+++ src/grid-utils.js
@@ -5,76 +5,15 @@ const maxGridGap = 100;
 const body = document.querySelector('body');
 var isEditable = false;
 var paddingGrid = window.innerWidth > 640 ? '0.9375rem' : '0.625rem';
-const styles = {
-    gridContainer:
-        `position: fixed;
-        top: 0;
-        left: 50%;
-        height: 100%;
-        width: 100%;
-        max-width: 75rem;
-        transform: translateX(-50%);
-        pointer-events: none;
-        display: grid;
-        grid-template-columns: repeat(${maxGridCol}, 1fr);
-        grid-gap: 30px;
-        z-index: 99999;
-        border: 1px solid orange;
-        padding: ${paddingGrid};`,
-    gridContainerFullWidth:
-        `position: fixed;
-        top: 0;
-        left: 50%;
-        height: 100%;
-        width: 100%;
-        max-width: 75rem;
-        transform: translateX(-50%);
-        pointer-events: none;
-        display: grid;
-        grid-template-columns: repeat(${maxGridCol}, 1fr);
-        grid-gap: 30px;
-        z-index: 99999;
-        padding: ${paddingGrid};
-        max-width: initial;
-        border: none;`,
-    gridColumn:
-        `opacity: 0.2;
-        background-color: #000000;`,
-    gridInfos:
-        `position: fixed;
-        top: 50%;
-        right: 0;
-        transform: translateY(-50%);
-        color: white;
-        background-color: rgba(0, 0, 0, 0.5);
-        padding: 15px;
-        z-index: 99999;`,
-    gridInfosItem:
-        `display: flex;
-        line-height: 1.5`,
-    gridInfosProperty:
-        `width: 90px;`,
-    cross:
-        `position: fixed;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 100%;
-        pointer-events: none;
-        z-index: 99999;`,
-    verticalLine:
-        `position: absolute;
-        left: 50%;
-        height: 100%;
-        width: 1px;
-        background: orange;`,
-    horizontalLine:
-        `position: absolute;
-        top: 50%;
-        height: 1px;
-        width: 100%;
-        background: orange;`
-};
+
+var css = `.grid { position: fixed; top: 0; left: 50%; height: 100%; width: 100%; max-width: 75rem; transform: translateX(-50%); pointer-events: none; display: grid; grid-template-columns: repeat(${maxGridCol}, 1fr); grid-gap: 30px; z-index: 99999; border: 1px solid orange; padding: ${paddingGrid}; } .grid--fullWidth { max-width: initial; border: none;} .grid__col { opacity: 0.15; background-color: #000000;} .gridInfos { position: fixed; top: 50%; right: 0; transform: translateY(-50%); color: white; background-color: rgba(0, 0, 0, 0.5); padding: 15px; z-index: 99999; } .gridInfos__item { display: flex;
+    line-height: 1.5; } .gridInfos__property__columns, .gridInfos__property__gutters, .gridInfos__property__fullWidth { width: 90px; } .cross { position: fixed; top: 0; left: 0; height: 100%; width: 100%; pointer-events: none; z-index: 99999; } .cross__verticalLine { position: absolute; left: 50%; height: 100%; width: 1px; background: orange; } .cross__horizontalLine { position: absolute; top: 50%; height: 1px; width: 100%; background: orange; }`;
+const head = document.getElementsByTagName('head')[0];
+var style = document.createElement('style');
+
+head.appendChild(style);
+
+style.appendChild(document.createTextNode(css));
 
 function gridHelper(event) {
     var pressedKey = event.key;
@@ -116,12 +55,10 @@ function appendGridHelper() {
         isEditable = true;
         var gridContainer = document.createElement('div');
         gridContainer.classList.add('grid');
-        gridContainer.style = styles.gridContainer;
 
         for (var i = 0; i < maxGridCol; i++) {
             var gridColumn = document.createElement('div');
             gridColumn.classList.add('grid__col');
-            gridColumn.style = styles.gridColumn;
             gridContainer.appendChild(gridColumn);
         }
 
@@ -135,15 +72,12 @@ function appendGridInfos() {
     if (!document.body.contains(document.querySelector('.gridInfos'))) {
         var gridInfos = document.createElement('div');
         gridInfos.classList.add('gridInfos');
-        gridInfos.style = styles.gridInfos;
 
         var gridInfosItem = document.createElement('div');
         gridInfosItem.classList.add('gridInfos__item');
-        gridInfosItem.style = styles.gridInfosItem;
 
         var gridInfosPropertyColumns = document.createElement('span');
         gridInfosPropertyColumns.classList.add('gridInfos__property__columns');
-        gridInfosPropertyColumns.style = styles.gridInfosProperty;
         gridInfosPropertyColumns.innerHTML = 'Columns :';
 
         var gridInfosNbColumns = document.createElement('span');
@@ -156,11 +90,9 @@ function appendGridInfos() {
 
         var gridInfosItem = document.createElement('div');
         gridInfosItem.classList.add('gridInfos__item');
-        gridInfosItem.style = styles.gridInfosItem;
 
         var gridInfosPropertyGutters = document.createElement('span');
         gridInfosPropertyGutters.classList.add('gridInfos__property__gutters');
-        gridInfosPropertyGutters.style = styles.gridInfosProperty;
         gridInfosPropertyGutters.innerHTML = 'Gutters :';
 
         var gridInfosNbGutters = document.createElement('span');
@@ -173,11 +105,9 @@ function appendGridInfos() {
 
         var gridInfosItem = document.createElement('div');
         gridInfosItem.classList.add('gridInfos__item');
-        gridInfosItem.style = styles.gridInfosItem;
 
         var gridInfosPropertyFullWidth = document.createElement('span');
         gridInfosPropertyFullWidth.classList.add('gridInfos__property__fullWidth');
-        gridInfosPropertyFullWidth.style = styles.gridInfosProperty;
         gridInfosPropertyFullWidth.innerHTML = 'Full width :';
 
         var gridInfosFullWidth = document.createElement('input');
@@ -199,14 +129,13 @@ function appendCross() {
     if (!document.body.contains(document.querySelector('.cross'))) {
         var crossContainer = document.createElement('div');
         crossContainer.classList.add('cross');
-        crossContainer.style = styles.cross;
 
         var verticalLine = document.createElement('div');
-        verticalLine.style = styles.verticalLine;
+        verticalLine.classList.add('cross__verticalLine');
         crossContainer.appendChild(verticalLine);
 
         var horizontalLine = document.createElement('div');
-        horizontalLine.style = styles.horizontalLine;
+        horizontalLine.classList.add('cross__horizontalLine');
         crossContainer.appendChild(horizontalLine);
 
         body.appendChild(crossContainer);
@@ -307,7 +236,6 @@ function increaseGridColumns() {
             if (columns < maxGridCol) {
                 var column = document.createElement('div');
                 column.classList.add('grid__col');
-                column.style = styles.gridColumn;
                 grid.appendChild(column);
                 columns = columns + 1;
                 grid.style.gridTemplateColumns = 'repeat(' + columns + ', 1fr)';
@@ -337,7 +265,7 @@ function limitGrid(property) {
     } else if (property == 'gutters') {
         var grid = document.querySelector('.grid');
 
-        grid.style.backgroundColor = 'rgba(255,0,0,0.2)';
+        grid.style.backgroundColor = 'rgba(255,0,0,0.15)';
     }
 }
 
@@ -364,8 +292,8 @@ function setFullWidthGrid() {
     var grid = document.querySelector('.grid');
 
     if (this.checked) {
-        grid.style = styles.gridContainerFullWidth;
+        grid.classList.add('grid--fullWidth');
     } else {
-        grid.style = styles.gridContainer;
+        grid.classList.remove('grid--fullWidth');
     }
 }
